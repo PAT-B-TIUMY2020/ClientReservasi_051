7a3437e6d994040d6b6a2c02026fa0dbf18c35ad
RifqiFirmasyah <56532181+UB8051@users.noreply.github.com>
2020-12-14 00:53:54 +0700

    pertemuan 11

---
+++ ClientReservasi_051/Login.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ClientReservasi_051
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.butttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(175, 76);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(189, 22);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(175, 148);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(189, 22);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // butttonLogin
+            // 
+            this.butttonLogin.Location = new System.Drawing.Point(289, 216);
+            this.butttonLogin.Name = "butttonLogin";
+            this.butttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.butttonLogin.TabIndex = 4;
+            this.butttonLogin.Text = "Login";
+            this.butttonLogin.UseVisualStyleBackColor = true;
+            this.butttonLogin.Click += new System.EventHandler(this.butttonLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butttonLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button butttonLogin;
+    }
+}
---
+++ ClientReservasi_051/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientReservasi_051
+{
+    public partial class Login : Form
+    {
+        ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void butttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+            string kategori = service.Login(username, password);
+
+            if (kategori == "Admin")
+            {
+                Register register = new Register();
+                this.Hide();
+                register.Show();
+            }
+            else if (kategori == "Resepsionis")
+            {
+                Form1 form1 = new Form1();
+                this.Hide();
+                form1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Username dan Password Tidak Cocok! Silahkan Hubungi Admin.");
+                textBoxUsername.Clear();
+                textBoxPassword.Clear();
+            }
+        }
+    }
+}
+
